36392b74af640721251c809e917f909919440234
mateiradu88 <mateiradu88@gmail.com>
2023-01-04 22:27:07 +0200

    ..

---
+++ src/main/java/ro/utcluj/helloworld/springboot/Controller/HelloController.java
@@ -0,0 +1,13 @@
+package ro.utcluj.helloworld.springboot.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String index() {
+        return "Hello World!";
+    }
+}
