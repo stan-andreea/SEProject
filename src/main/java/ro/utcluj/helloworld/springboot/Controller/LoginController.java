23e1e9db256379e15785f854c91a4e562035aa6f
mateiradu88 <mateiradu88@gmail.com>
2023-01-05 19:35:45 +0200

    .

---
+++ src/main/java/ro/utcluj/helloworld/springboot/Controller/LoginController.java
@@ -0,0 +1,15 @@
+package ro.utcluj.helloworld.springboot.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+    @RequestMapping("/")
+    public String loginPage() {
+        return "login";
+    }
+
+}
